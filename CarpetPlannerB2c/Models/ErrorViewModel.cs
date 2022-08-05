396fb5656a1bc173f3dec37e1ad4e4b65ad7c008
Matti Pulkkinen <matti@pulkkinen.tv>
2022-08-05 22:33:39 +0300

    Add CarpetPlannerB2c project

---
+++ CarpetPlannerB2c/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarpetPlannerB2c.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
