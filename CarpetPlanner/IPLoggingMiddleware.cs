6f4d7c682474a57e110c33bd597cebb8b7b00b9a
Matti Pulkkinen <matti@pulkkinen.tv>
2025-05-22 11:51:18 +0300

    Revert "Add header logging to IPLoggingMiddleware"
    This reverts commit fb08049bba4da2c949ea3b2dcdd6b9f590101b79.

--- CarpetPlanner/IPLoggingMiddleware.cs
+++ CarpetPlanner/IPLoggingMiddleware.cs
@@ -13,10 +13,6 @@ public class IPLoggingMiddleware(RequestDelegate next, ILogger<IPLoggingMiddlewa
     {
         var ipAddress = context.Connection.RemoteIpAddress?.ToString();
         _logger.LogInformation("Incoming request from IP: {IPAddress}", ipAddress);
-        foreach (var header in context.Request.Headers)
-        {
-            _logger.LogInformation("Header: {Header} = {Value}", header.Key, header.Value);
-        }
 
         // Call the next delegate/middleware in the pipeline
         await _next(context);
